73b3e3939ce5ae9ae2a9eb71ec9933936886c516
Jithin Balan <45451221+jithinbalan@users.noreply.github.com>
2022-07-04 02:37:47 +0100

    port changed to3001

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ app.use('/api/admin', adminRouter)
 
 //Port
 
-const PORT = process.env.PORT || 3001
+const PORT =  3001
 
 //Server
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
